d8735415d654ea6be741b9cc25c71ef4fefc31ec
Saman Waruka <t555136@scbcorp.co.th>
2024-06-27 15:13:37 +0700

    feat: add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { CREDENTIAL } from "@/constants/keyValue";
+import { ROUTE } from "@/constants/route";
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  console.log(" Activate middleware ", request.url);
+  // console.log("headers", request.headers);
+
+  try {
+    const cookieToken = request.cookies.get(CREDENTIAL.ACCESS_TOKEN);
+    if (!cookieToken) {
+      const url = new URL(ROUTE.LOGIN, request.url);
+      return NextResponse.redirect(url);
+    }
+
+    const token = cookieToken.value;
+  } catch (error) {
+    const url = new URL(ROUTE.LOGIN, request.url);
+    console.error("Middleware error\n", error);
+    NextResponse.redirect(url);
+  }
+}
+
+// specify the path regex to apply the middleware to
+export const config = {
+  matcher: ["/", `/todos/:path*`],
+};
