c87de131ee0f2f4ae1d636245cc19724d235f1d4
Saman Waruka <t555136@scbcorp.co.th>
2024-06-27 15:17:16 +0700

    feat: update app page, layout, error page

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export default function Error() {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      🥶 Something went wrong, pleas try again. 🤬
+    </div>
+  );
+}
