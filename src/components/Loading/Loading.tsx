833fc352ee1b3d3e5a2d0c7cdd80abd4873fa514
Saman Waruka <t555136@scbcorp.co.th>
2024-06-28 08:44:04 +0700

    feat: implement loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="w-full  flex justify-center items-center content-center">
+      Loading...
+    </div>
+  );
+}
